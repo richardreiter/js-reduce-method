11c70c9f1720be1b90a7568037b9732cadf5bc27
richardreiter <richard.reiter.dev@gmail.com>
2022-01-31 23:07:32 +0000

    Practice

--- reduce.js
+++ reduce.js
@@ -6,7 +6,18 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => {
+//   console.log(
+//     'Accumulator:', acc,
+//     'Current Value:', curr,
+//     'Total:', acc + curr
+//   );
+//   return acc + curr;
+// }, 0);
 
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
   {
@@ -32,6 +43,19 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
